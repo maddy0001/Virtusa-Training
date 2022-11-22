2a0c46b21f43b4237d2a4bbd4f4919b08b55ae0d
maddy0001 <87888421+maddy0001@users.noreply.github.com>
2022-11-22 22:11:52 +0530

    Add files via upload

---
+++ Selenium Automation Practice Codes/Seleniumpractice1.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Seleniumpractice1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		// driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.amazon.com/");
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id=\"nav-main\"]/div[1]/div/div/div[3]/span[1]/span/input")).click();
+		WebElement findElement = driver.findElement(By.id("twotabsearchtextbox"));
+		findElement.click();
+		findElement.sendKeys("Asus rog 5");
+		Thread.sleep(3000);
+		driver.findElement(By.id("nav-search-submit-button")).click();
+		driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]/div[2]/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span")).click();
+		driver.findElement(By.xpath("//*[@id=\"buybox-see-all-buying-choices\"]/span/a")).click();
+}
+}
